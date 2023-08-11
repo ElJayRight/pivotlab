7732c5a07cbffb632863ff6c20e1671bf53877eb
eljayright <liamwri2003@hotmail.com>
2023-08-11 20:21:42 +1000

    v1.9 added a few restrictions to make things harder.

---
+++ server.py
@@ -0,0 +1,21 @@
+import http.server
+import socketserver
+
+class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        client_ip = self.client_address[0]
+        if client_ip=="127.0.0.1":
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"HEY THATS CHEATING.\nPlease stop.\nI wrote a custom GET python server just so you can't do this.\nGood job for trying tho!")
+        else:
+            super().do_GET()
+
+if __name__ == "__main__":
+    host = "127.0.0.1"  # Loopback address
+    port = 8080
+
+    with socketserver.TCPServer((host, port), CustomRequestHandler) as httpd:
+        print(f"Server listening on http://{host}:{port}")
+        httpd.serve_forever()
