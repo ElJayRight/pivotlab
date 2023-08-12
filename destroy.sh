a31adc023202ada382de4d80679d452d3527ba1b
eljayright <liamwri2003@hotmail.com>
2023-08-13 00:46:14 +1000

    v2.0.1 fixed flag format.

--- destroy.sh
+++ destroy.sh
@@ -3,3 +3,9 @@ sudo docker rm admin_box internal_box public_box -f
 
 echo "=== Removing docker networks ==="
 sudo docker network prune -f
+
+echo "=== Removing verified keys ==="
+ssh-keygen -f "/home/eljay/.ssh/known_hosts" -R "10.10.16.3"
+ssh-keygen -f "/home/eljay/.ssh/known_hosts" -R "10.10.4.3"
+ssh-keygen -f "/home/eljay/.ssh/known_hosts" -R "192.168.69.2"
+
