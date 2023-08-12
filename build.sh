bd587e4a35143ab55e4c6d172c996a43ae1d6280
eljayright <liamwri2003@hotmail.com>
2023-08-13 02:08:56 +1000

    v2.1 Now with half a lab guide.

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
+echo "=== Building the lab ==="
 python3 -m pip install ansible-core
 ansible-galaxy collection install community.docker
 ansible-playbook playbook.yml
 
+echo "=== Adding iptables rules ==="
+sudo iptables -A INPUT -s 10.10.16.3 -j DROP
+sudo iptables -A OUTPUT -s 10.10.16.3 -j DROP
+
+sudo iptables -A INPUT -s 10.10.4.3 -j DROP
+sudo iptables -A INPUT -s 10.10.4.3 -j DROP
+
+echo "=== LAB INFO ==="
 sudo docker inspect public_box| grep IPAddress|tail -1 | awk -F\" '{print "Entry point: " $4}'
 echo "Credentials:"
 echo "  - public_box -> danny:danny"
--- destroy.sh
+++ destroy.sh
@@ -9,3 +9,9 @@ ssh-keygen -f "/home/eljay/.ssh/known_hosts" -R "10.10.16.3"
 ssh-keygen -f "/home/eljay/.ssh/known_hosts" -R "10.10.4.3"
 ssh-keygen -f "/home/eljay/.ssh/known_hosts" -R "192.168.69.2"
 
+echo "=== Removing iptable rules ==="
+sudo iptables -D INPUT -s 10.10.16.3 -j DROP
+sudo iptables -D OUTPUT -s 10.10.16.3 -j DROP
+
+sudo iptables -D INPUT -s 10.10.4.3 -j DROP
+sudo iptables -D INPUT -s 10.10.4.3 -j DROP
